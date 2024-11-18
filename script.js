d8dac2dbb2d9ee7c7f7213063910af56ef5731af
GabrielLogan-prog <gabriel@teiax.com>
2024-11-17 22:14:59 -0300

    iniciando a função de consumo da API via fetch

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
- 
+ /**
+ * URL of the API.
+ * OBS , SE REMOVER O REDIRECIONAMENTO PELO HEROKU O REPLIT NAO LIBERA ACESSO A API 
+ * @constant
+ * @type {string}
+ */
+const API_URL = 'https://cors-anywhere.herokuapp.com/https://c9df0f37-f69f-4b6d-8158-2958f9b5b884-00-1wdt1mzj7jyv1.worf.replit.dev/api/company';
